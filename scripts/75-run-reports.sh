c95641f7dc0ee021be62db8faaa5d5b79eae0ff1
Bruce W. Herr II <bherr@indiana.edu>
2023-11-18 01:30:42 -0500

    Added code to start and stop the blazegraph runner for running reports

--- scripts/75-run-reports.sh
+++ scripts/75-run-reports.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 source constants.sh
 shopt -s extglob
-set -ev
+# set -ev
 
 DIR=$RAW_DIR/$VERSION
 FILTER="$1"
 
 # SPARQL endpoint with hra-tpop data loaded
-SPARQL_ENDPOINT=https://lod.humanatlas.io/sparql
+#SPARQL_ENDPOINT=https://lod.humanatlas.io/sparql
 # SPARQL_ENDPOINT=https://ukiemb6svh.us-east-2.awsapprunner.com/blazegraph/namespace/kb/sparql
-# SPARQL_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
+SPARQL_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
-#node ./src/run-reports.js $SPARQL_ENDPOINT $RAW_DIR/$VERSION/reports $FILTER
+docker compose up -d
+sleep 1
+
+node ./src/run-reports.js $SPARQL_ENDPOINT $RAW_DIR/$VERSION/reports $FILTER
+
+sleep 1
+docker compose down -t 5
