c4389782114d4957e6fb58f89da24a020637f320
Bruce W. Herr II <bherr@indiana.edu>
2023-11-18 04:11:32 -0500

    Uncomment

--- scripts/75-run-reports.sh
+++ scripts/75-run-reports.sh
@@ -7,7 +7,7 @@ DIR=$RAW_DIR/$VERSION
 FILTER="$1"
 
 # SPARQL endpoint with hra-tpop data loaded
-#SPARQL_ENDPOINT=https://lod.humanatlas.io/sparql
+# SPARQL_ENDPOINT=https://lod.humanatlas.io/sparql
 # SPARQL_ENDPOINT=https://ukiemb6svh.us-east-2.awsapprunner.com/blazegraph/namespace/kb/sparql
 SPARQL_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
