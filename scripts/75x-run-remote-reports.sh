f7e6da95cb697aa6b98a5516a34c6ab8de3f6b95
Bruce W. Herr II <bherr@indiana.edu>
2023-11-30 12:10:26 -0500

    Add script to re-run remotes after processing

---
+++ scripts/75x-run-remote-reports.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source constants.sh
+shopt -s extglob
+# set -ev
+
+DIR=$OUTPUT_DIR/$VERSION
+FILTER="$1"
+
+# SPARQL endpoint with hra-pop data loaded
+SPARQL_ENDPOINT=https://lod.humanatlas.io/sparql
+
+node ./src/run-reports.js $SPARQL_ENDPOINT $DIR/reports $FILTER
