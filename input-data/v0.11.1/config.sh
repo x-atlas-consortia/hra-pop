ff52915c9f6020e11a1d56d79f205845e882eddd
Bruce W. Herr II <bherr@iu.edu>
2024-11-04 16:27:03 -0500

    Update input summaries for v0.11.1

--- input-data/v0.11.1/config.sh
+++ input-data/v0.11.1/config.sh
@@ -1,9 +1,5 @@
 VERSION="v0.11.1"
 
-DEPRECATED_SPATIAL_CELL_SUMMARIES=input-data/${VERSION}/spatial-cell-summaries.jsonld
-DEPRECATED_SPATIAL_CELL_SUMMARY_DATASETS=input-data/${VERSION}/spatial-dataset-metadata.csv
-DEPRECATED_CORRIDORS=raw-data/${VERSION}/corridors.jsonld
-
 REGISTRATIONS="https://apps.humanatlas.io/api/ds-graph/hubmap?token=${HUBMAP_TOKEN} \
   https://apps.humanatlas.io/api/ds-graph/sennet?token=${SENNET_TOKEN} \
   https://apps.humanatlas.io/api/ds-graph/gtex \
@@ -12,5 +8,5 @@ REGISTRATIONS="https://apps.humanatlas.io/api/ds-graph/hubmap?token=${HUBMAP_TOK
 PUBLICATIONS="https://docs.google.com/spreadsheets/d/1MK9KMPAFRHN6aU_e1ghApcbABAdkLGRTiaSUkliLNnw/export?format=csv&gid=1240375199"
 APPROVED_SOURCES="HuBMAP SenNet GTEx"
 
-CELL_SUMMARIES="input-data/${VERSION}/bulk-cell-summaries.jsonld.gz input-data/${VERSION}/spatial-cell-summaries.jsonld"
-CELL_SUMMARY_DATASETS="input-data/${VERSION}/bulk-dataset-metadata.csv input-data/${VERSION}/spatial-dataset-metadata.csv"
+CELL_SUMMARIES="input-data/${VERSION}/sc-transcriptomics-cell-summaries.jsonld.gz input-data/${VERSION}/sc-proteomics-cell-summaries.jsonld"
+CELL_SUMMARY_DATASETS="input-data/${VERSION}/sc-transcriptomics-dataset-metadata.csv input-data/${VERSION}/sc-proteomics-dataset-metadata.csv"
