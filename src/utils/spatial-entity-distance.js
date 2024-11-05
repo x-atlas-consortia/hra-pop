6a91c3cf3da09822d5c1102d8cdb4723255829ba
Bruce W. Herr II <bherr@iu.edu>
2024-11-05 08:34:51 -0500

    Use the latest HRA API when finding common coordinates

--- src/utils/spatial-entity-distance.js
+++ src/utils/spatial-entity-distance.js
@@ -1,7 +1,7 @@
 import { distance } from 'mathjs';
 
-const SPATIAL_PLACEMENT_LOOKUP = 'https://apps.humanatlas.io/hra-api/v1/get-spatial-placement';
-const GLOBAL_ENTITY_ID = 'http://purl.org/ccf/latest/ccf.owl#VHBothSexes';
+const SPATIAL_PLACEMENT_LOOKUP = 'https://apps.humanatlas.io/api/v1/get-spatial-placement';
+const GLOBAL_ENTITY_ID = 'https://purl.humanatlas.io/graph/hra-ccf-body#VHBothSexes';
 const GLOBAL_POINT_CACHE = {};
 
 async function getGlobalPoint(entity) {
@@ -40,15 +40,20 @@ export async function getSpatialEntityDistance(entityA, entityB) {
 
   let pointA;
   let pointB;
-  if (targetA === targetB) {
-    pointA = getLocalPoint(entityA);
-    pointB = getLocalPoint(entityB);
-  } else {
-    pointA = await getGlobalPoint(entityA);
-    pointB = await getGlobalPoint(entityB);
-  }
 
-  return distance(pointA, pointB);
+  try {
+    if (targetA === targetB) {
+      pointA = getLocalPoint(entityA);
+      pointB = getLocalPoint(entityB);
+    } else {
+      pointA = await getGlobalPoint(entityA);
+      pointB = await getGlobalPoint(entityB);
+    }
+    return distance(pointA, pointB);
+  } catch (err) {
+    console.log(JSON.stringify({ message: err.message, entityA, entityB, pointA, pointB }, null, 2));
+    return Number.MAX_SAFE_INTEGER;
+  }
 }
 
 export async function* getAllSpatialEntityDistances(entities, maxDistance = 10000) {
