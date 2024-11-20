f07935cece9973d4e08ac2880face9215ee8cdc9
Bruce W. Herr II <bherr@iu.edu>
2024-11-20 15:06:59 -0500

    Update vccf-federated location

--- input-data/v0.11.1/config.sh
+++ input-data/v0.11.1/config.sh
@@ -4,7 +4,7 @@ REGISTRATIONS="https://apps.humanatlas.io/api/ds-graph/hubmap?token=${HUBMAP_TOK
   https://apps.humanatlas.io/api/ds-graph/sennet?token=${SENNET_TOKEN} \
   https://apps.humanatlas.io/api/ds-graph/gtex \
   https://hubmapconsortium.github.io/hra-registrations/federated/rui_locations.jsonld \
-  https://hubmapconsortium.github.io/hra-registrations/staging/vccf-federated/rui_locations.jsonld \
+  https://hubmapconsortium.github.io/hra-registrations/vccf-federated/rui_locations.jsonld \
   https://hubmapconsortium.github.io/hra-registrations/staging/kpmp-kidney-jain-2023/rui_locations.jsonld"
 
 PUBLICATIONS="https://docs.google.com/spreadsheets/d/1MK9KMPAFRHN6aU_e1ghApcbABAdkLGRTiaSUkliLNnw/export?format=csv&gid=1240375199"
